9e4ddf3fe25ee424310ffe33b77211a4269aaad8
CosmicOppai <oppaiharem69@gmail.com>
2021-11-19 15:25:51 +0530

    added the batch transaction

--- banking/commands.py
+++ banking/commands.py
@@ -1,5 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from banking.account import Account
+from banking.transaction import Transaction
 
 
 @dataclass
@@ -70,3 +71,26 @@ class Transfer:
         self.from_account.withdraw(self.amount)
         self.to_account.deposit(self.amount)
         print(f"Redid Transfer of {self.transaction_details}")
+
+
+@dataclass
+class Batch:
+    commands: list[Transaction] = field(default_factory=list)
+
+    def execute(self):
+        completed_commands: list[Transaction] = []
+        try:
+            for command in self.commands:
+                command.execute()
+                completed_commands.append(command)
+        except ValueError:
+            for command in reversed(completed_commands):
+                command.undo()
+
+    def undo(self) -> None:
+        for command in reversed(self.commands):
+            command.undo()
+
+    def redo(self) -> None:
+        for command in self.commands:
+            command.execute()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from banking.bank import Bank
 from banking.controller import BankController
-from banking.commands import Deposit, Withdrawal, Transfer
+from banking.commands import Deposit, Withdrawal, Transfer, Batch
 
 
 def main() -> None:
@@ -14,17 +14,18 @@ def main() -> None:
     account2 = bank.create_account("CosmicOppai")
     account3 = bank.create_account("The SayAnime")
 
-    controller.execute(Deposit(account1, 500))
-    controller.execute(Deposit(account2, 5000))
-    controller.execute(Deposit(account3, 50000))
-
-    controller.execute(Withdrawal(account1, 10))
-    controller.execute(Withdrawal(account2, 100))
-    controller.execute(Withdrawal(account3, 10000))
-
-    controller.execute(Transfer(from_account=account1, to_account=account3, amount=100))
-    controller.execute(Transfer(from_account=account2, to_account=account3, amount=400))
-    controller.undo()
+    controller.execute((Batch(
+        commands=[
+            Deposit(account1, 500),
+            Deposit(account2, 5000),
+            Deposit(account3, 50000),
+            Withdrawal(account1, 10),
+            Withdrawal(account2, 100),
+            Withdrawal(account3, 10000),
+            Transfer(from_account=account1, to_account=account3, amount=100),
+            Transfer(from_account=account2, to_account=account3, amount=400),
+        ]
+    )))
     print(bank)
 
 
